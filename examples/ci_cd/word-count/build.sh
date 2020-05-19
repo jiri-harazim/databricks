c1d4a3db8fe74811307fbb419d8ff1ba34e08468
Jiri Harazim <jiri.harazim@databricks.com>
2020-05-19 18:04:00 +0200

    remove typo line

--- examples/ci_cd/word-count/build.sh
+++ examples/ci_cd/word-count/build.sh
@@ -6,5 +6,4 @@ python setup.py sdist bdist_wheel
 echo "Current dir: $(pwd)"
 mv dist/word_count-0.0.post0.*-py2.py3-none-any.whl dist/word_count-0.0.1-py2.py3-none-any.whl
 echo "ls dist: $(ls dist)"
-/home/vsts/work/1/s/dist/word_count-0.0.1-py2.py3-none-any.whl
 
