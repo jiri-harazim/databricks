240e65de258a108ed9f280f612c9aef18921729c
Jiri Harazim <jiri.harazim@databricks.com>
2020-05-19 18:14:38 +0200

    add debug echo

--- examples/ci_cd/word-count/build.sh
+++ examples/ci_cd/word-count/build.sh
@@ -5,5 +5,5 @@ pip install wheel
 python setup.py sdist bdist_wheel
 echo "Current dir: $(pwd)"
 mv dist/word_count-0.0.post0.*-py2.py3-none-any.whl dist/word_count-0.0.1-py2.py3-none-any.whl
-echo "ls dist: $(ls dist)"
+echo "ls dist: $(ls dist) ls build: $(ls build) ls cov_report: $(ls cov_html)"
 
