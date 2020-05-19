f1f07d2d8f6d9135b0a47b59f2793e0c87bb0dae
Jiri Harazim <jiri.harazim@databricks.com>
2020-05-19 15:09:49 +0200

    rename to count, cleanup and wheel renamed

--- examples/ci_cd/word-count/build.sh
+++ examples/ci_cd/word-count/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+set -x
 python setup.py test
 pip install wheel
 python setup.py sdist bdist_wheel
+echo "Current dir: $(pwd)"
 mv dist/word_count-0.0.post0.*-py2.py3-none-any.whl dist/word_count-0.0.1-py2.py3-none-any.whl
+echo "ls dist: $(ls dist)"
+
